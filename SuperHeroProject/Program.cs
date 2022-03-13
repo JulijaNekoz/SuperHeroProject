dbc7cf1f880b9be9d7381ea0f08a09de648479c1
Jul Nek <julijanekoz@yahoo.co.uk>
2022-03-13 19:36:53 +0200

    Salary calculator (Session 7)

--- SuperHeroProject/Program.cs
+++ SuperHeroProject/Program.cs
@@ -1,9 +1,25 @@
 ﻿
+Console.WriteLine("**********SALARY CALCULATOR************");
 
-string name = "Groot";
-int age = 25;
-string ability1 = "Superhuman strength";
-string ability2 = "Enhanced speed";
-string ability3 = "Immortality";
+Console.WriteLine("Please, enter the amount of working hours in a day:");
+string input = Console.ReadLine();
+if (double.TryParse(input, out double workingHoursInDay) == false)
+{
+    Console.WriteLine("Please, enter a valid amount of daily working hours!");
+    return;
+}
 
-Console.WriteLine(**************************);
+if (workingHoursInDay < 0 || workingHoursInDay > 24)
+{
+    Console.WriteLine("The number for the amount of daily working hours is invalid. Try again.");
+    return;
+}
+
+if (workingHoursInDay <= 8)
+{
+    Console.WriteLine($"Salary per day is {workingHoursInDay * 10}");
+}
+else
+{ 
+    Console.WriteLine($"Salary per day is {80 + (workingHoursInDay - 8) * 15} euros");
+}
