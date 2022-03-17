83b1d63b82d4e1391c1efebb68f1b3d0bf77f817
Jul Nek <julijanekoz@yahoo.co.uk>
2022-03-17 20:24:47 +0200

    clear

--- SuperHeroProject/Program.cs
+++ SuperHeroProject/Program.cs
@@ -1,9 +1,1 @@
 ﻿
-
-string name = "Groot";
-int age = 25;
-string ability1 = "Superhuman strength";
-string ability2 = "Enhanced speed";
-string ability3 = "Immortality";
-
-Console.WriteLine(**************************);
